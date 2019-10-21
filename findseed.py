ddd2faeecfc5600583dda2ab2273de5863633649
Yannis <cattan.yannis@gmail.com>
2019-10-21 19:27:32 +0200

    coucou

--- findseed.py
+++ findseed.py
@@ -24,15 +24,21 @@ def find_seed(f,c=0.0,x=0.0,eps=2**(-26)):
 
 def distance(x,y):
     return np.sqrt(((x[0]-y[0]**2)+(x[1]-y[1]])**2))
+
 def simple_contour(f,c=0.0,delta=0.01):
     x=[]
     y=[]
     Stock=[]
+    distance=[]
     for i in np.arange(0,1,delta):
         for j in np.arange(i,i+delta,delta/1000):
            if find_seed(f,c,j) != None :
                Stock+=[j]
-               
+               for a in Stock :
+                   distance+=[distance(j,find_seed(f,c,j))]
+                   
+
+
         
         y+=[find_seed(f,c,i)]
         x+=[i]
