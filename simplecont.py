31eab7de41965b06cca99b074fdc2918884d2451
Yannis <cattan.yannis@gmail.com>
2019-10-22 14:40:10 +0200

    ajout fonction simple_contour avec methode gradient

---
+++ simplecont.py
@@ -0,0 +1,24 @@
+import numpy as np  
+import matplotlib.pypylot as plt  
+
+def find_seed(f,c=0.0,x=0.0,eps=2**(-26)):
+    if not f(x,1.0)<= c <= f(x,0.0) and not f(x,0)<=c<=f(x,1.0):
+            return None  
+    a=0
+    b=1
+    t=0.5
+    while abs(f(x,t)-c)>eps:  ##dichotomie des familles mais Newton ne fonctionnait pas...
+        if (f(x,a)-c)*(f(x,t)-c)>=0:
+            a=t
+        else :
+            b=t
+        t=(a+b)/2
+    return t 
+
+def derivee_prem_coord(f,x,y,h=10**-8):
+    return (f(x+h,y)-f(x,y))/h
+
+def derivee_deux_coord(f,x,y,h=10**-8):
+    return (f(x,y+h)-f(x,y))/h
+
+def simple_contour(f,c=0.0,delta=0.01):
