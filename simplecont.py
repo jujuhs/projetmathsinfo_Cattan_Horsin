00bdfa7cbfab0832f6ff20d940832d6c4d6ba3c6
Julien Horsin <julien.horsin@mines-paristech.fr>
2019-10-22 15:18:04 +0200

    on avance

--- simplecont.py
+++ simplecont.py
@@ -1,5 +1,5 @@
 import numpy as np  
-import matplotlib.pypylot as plt  
+import matplotlib.pyplot as plt  
 
 def find_seed(f,c=0.0,x=0.0,eps=2**(-26)):
     if not f(x,1.0)<= c <= f(x,0.0) and not f(x,0)<=c<=f(x,1.0):
@@ -14,6 +14,13 @@ def find_seed(f,c=0.0,x=0.0,eps=2**(-26)):
             b=t
         t=(a+b)/2
     return t 
+def f(x,y):
+    return 2*(np.exp(-x**2-y**2)-np.exp(-(x-1)**2-(y-1)**2))
+def normalisation(vecteur,normev):
+    norme=distance(vecteur,(0,0))
+    x=vecteur[0]
+    y=vecteur[1]
+    return (x*normev/norme,y*normev/norme)
 
 def derivee_prem_coord(f,x,y,h=10**-8):
     return (f(x+h,y)-f(x,y))/h
@@ -21,4 +28,33 @@ def derivee_prem_coord(f,x,y,h=10**-8):
 def derivee_deux_coord(f,x,y,h=10**-8):
     return (f(x,y+h)-f(x,y))/h
 
+def distanceeucl(x,y):
+    return np.sqrt(((x[0]-y[0])**2)+((x[1]-y[1])**2))
+
 def simple_contour(f,c=0.0,delta=0.01):
+    x=[0]
+    y=[find_seed(f,c,0)]          
+    while x[-1]<1-delta and y[-1]>0+delta and y[-1]<0-delta:
+        distance=[]
+        avant=[x[-1],y[-1]]
+        tang=(-derivee_deux_coord(f,avant[0],avant[1]),derivee_prem_coord(f,avant[0],avant[1]))
+        v=normalisation(tang,delta)
+        p1=(avant[0]+v[0],avant[1]+v[0])
+        p2=(avant[0]-v[0],avant[1]-v[0])
+        p=[p1,p2]
+        for i in p:
+            distance+=[distanceeucl((x[-2],y[-2]),i)]
+        e=p.index(max(p))
+        x.append(p[e][0])
+        y.append(p[e][1])
+    print([x,y])
+    return [x,y]
+c=input("Donnez la valeur du réel c >> ")    
+data=simple_contour(f,float(c))
+#print(simple_contour(f,float(c)))
+plt.plot(data[0],data[1])
+plt.grid()
+plt.title(f"Courbe de niveau pour c={float(c)}")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
